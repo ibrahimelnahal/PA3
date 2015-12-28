537bc8ddfa27361563488c03ddf82e7c78b34092
IBRAHIM ELNAHAL <ibrahimelnahal@Supports-MacBook.local>
2015-12-29 00:46:08 +0200

    Initial commit

---
+++ PA3/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from PA3.models import *
+
+
+class StudentForm(forms.Form):
+    first_name=forms.CharField(max_length=30)
+    last_name=forms.CharField(max_length=30)
+    email = forms.EmailField()
+    #courses = models.ManyToManyField(Course)
+
+
+
+class TeacherForm(forms.Form):
+    first_name=forms.CharField(max_length=30)
+    last_name=forms.CharField(max_length=30)
+    office=forms.CharField(max_length=15)
+    email = forms.EmailField()
+    phone=forms.CharField(max_length=13)
+
+
+
+class CourseForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    code = forms.CharField(max_length=6)
+    time=forms.TimeField()
+    classroom=forms.CharField(max_length=6)
+    teacher = forms.ModelChoiceField(queryset = Teacher.objects.all())
+    #students = forms.ManyToManyField(Student)
+
+"""
+class studentincourseForm(forms.Form):                 #By this I can enrol students to courses. I added one Form more.
+    courses =  forms.ModelChoiceField(queryset = Course.objects.all())
+    students = forms.ModelChoiceField(queryset = Student.objects.all())
+
+
+"""
